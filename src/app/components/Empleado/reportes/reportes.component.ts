99bb327032e923d6501137a554e24969b5f3c1a6
dontpan-ik <64174091+dontpan-ik@users.noreply.github.com>
2020-11-28 21:59:58 -0600

    imprime Todos Pedidos falta acomodar

--- src/app/components/Empleado/reportes/reportes.component.ts
+++ src/app/components/Empleado/reportes/reportes.component.ts
@@ -17,13 +17,26 @@ export class ReportesComponent implements OnInit {
 
   pedidos;
 
+  noPedidos;
+
+  total=0;
+
+
   filtros = {
     nombre : "",
     categoria : "Todo",
     subCategoria : "Todos"
   }
 
-  constructor(private pedidoService : PedidoService) { }
+  productosInfo = [{
+    cantidadProd : 0,
+    monto : 0,
+    total : 0,
+    nombreProd :'',
+    precio : 0
+  }];
+
+  constructor(private pedidoService : PedidoService, private productoService : InventarioService) { }
 
   ngOnInit(): void {
     this.totalPedidos()
@@ -33,8 +46,20 @@ export class ReportesComponent implements OnInit {
   totalPedidos(){
     this.pedidoService.consultarTodo().subscribe(res =>{
       this.pedidos=res;
+      this.noPedidos=this.pedidos.length;
+      for(let i=0;i<this.noPedidos;i++){
+        if(this.pedidos[i]!=null){
+          this.total+=this.pedidos[i].total;
+        }
+      }
+      console.log(this.total);
     },
     err => console.log(err));
+    
+  }
+
+  infoReporte(){
+
   }
 
   generarPDF(){       
@@ -45,21 +70,49 @@ export class ReportesComponent implements OnInit {
     new Img('../assets/img/logo_crem_adap.png').alignment('center').height(80).width(80).build().then( img => {
     pdf.add( img );
     
-    pdf.create().download();
+    //pdf.create().download();
 
-    
-    
+    for(let i=0;i<this.noPedidos;i++){
 
-    /*for (let i = 0; i < this.pedidoC.tiene.length; i++) { 
       pdf.add(
-        new Columns([this.productosInfo[i].nombreProd,'$' +this.productosInfo[i].precio ,this.productosInfo[i].cantidadProd,'$' +this.productosInfo[i].monto]).fontSize(10).margin([0,10,0,0]).alignment('center').end
-      ) 
+        new Columns(['Total del Pedido: ', this.pedidos[i].total]).fontSize(12).margin([0,10,0,0]).end
+      )
+      pdf.add(
+        new Columns(['Metodo de pago: ', this.pedidos[i].metodoPago]).fontSize(12).margin([0,10,0,0]).end
+      )
+  
+      pdf.add(
+        new Columns(['Direccion: ', this.pedidos[i].estatus]).fontSize(12).margin([0,10,0,0]).end
+      )
+  
+      pdf.add(
+        new Columns(['Fecha entrega: ', this.pedidos[i].fechaEntrega]).fontSize(12).margin([0,10,0,0]).end
+      )
+  
+      pdf.add(
+        new Columns(['Estatus: ', this.pedidos[i].metodoPago]).fontSize(12).margin([0,10,0,0]).end
+      )
+  
+      pdf.add(
+        new Txt('Productos del pedido').bold().alignment("center").fontSize(14).margin([0,10,0,0]).end
+      )
+  
+      pdf.add(
+        new Columns(['Producto','Precio','Cantidad','monto']).fontSize(12).margin([0,10,0,0]).bold().background('#F9E79F').alignment('center').end
+      )
+      for (let j = 0; j < this.pedidos[i].tiene.length; j++) {
+
+        pdf.add('--Producto INFO--');
+        /*pdf.add(
+          new Columns([this.productosInfo[j].nombreProd,'$' +this.productosInfo[j].precio ,this.productosInfo[j].cantidadProd,'$' +this.productosInfo[j].monto]).fontSize(10).margin([0,10,0,0]).alignment('center').end
+        )*/
+      }
+      pdf.add(
+        new Txt('Total: $' + this.pedidos[i].total).bold().alignment("right").fontSize(14).margin([0,20,0,0]).end
+      )
     }
 
-    pdf.add(
-      new Txt('Total: $' + this.pedidoC.total).bold().alignment("right").fontSize(14).margin([0,20,0,0]).end
-    )*/
-
+    pdf.create().download();
 
     pdf.create().open();
   });
