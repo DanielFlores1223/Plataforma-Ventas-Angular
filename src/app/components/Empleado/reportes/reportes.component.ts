83c22dac691d35d284c0cc17cb99840652b11be4
dontpan-ik <64174091+dontpan-ik@users.noreply.github.com>
2020-11-30 10:01:57 -0600

    reporte por estatus completo

--- src/app/components/Empleado/reportes/reportes.component.ts
+++ src/app/components/Empleado/reportes/reportes.component.ts
@@ -19,7 +19,10 @@ export class ReportesComponent implements OnInit {
   noPedidos;
 
   total=0;
+  filtro="Todas";
+  tipo="Total";
 
+  periodo =["dia","semana","mes","anio"];
 
   filtros = {
     nombre : "",
@@ -80,6 +83,38 @@ export class ReportesComponent implements OnInit {
 
   }
 
+  buscar(){
+    if(this.tipo=="Total"){
+      console.log('La variable es: '+this.tipo)
+    }
+    else{
+      console.log("Saludos perro"+this.tipo);
+    }
+  }
+
+  actualizar(){
+    this.total=0;
+    this.noPedidos=0;
+    // it could be some trouble here, just be careful
+    if(this.filtro!="Todas"){
+      for(let i=0;i<this.pedidos.length;i++){
+        if(this.pedidos[i].estatus==this.filtro){
+          this.noPedidos++;
+          if(this.pedidos[i]!=null){
+            this.total+=this.pedidos[i].total;
+          }
+        }
+      }
+    }else{
+      this.noPedidos=this.pedidos.length;
+      for(let i=0;i<this.noPedidos;i++){
+        if(this.pedidos[i]!=null){
+          this.total+=this.pedidos[i].total;
+        }
+      }
+    }
+  }
+
   generarPDF(){       
     const pdf = new PdfMakeWrapper();
 
@@ -95,62 +130,105 @@ export class ReportesComponent implements OnInit {
       new Txt('Reporte de Pedidos').alignment('center').fontSize(15).bold().end
     );
     pdf.add(pdf.ln());
-    for(let i=0;i<this.noPedidos;i++){
-
-      pdf.add(
-        new Table([
-          [ new Txt('Estatus').bold().end,
-          new Txt('Metodo de pago').bold().end,
-          new Txt('Direccion').bold().end,
-          new Txt('Fecha entrega').bold().end],
-          [ new Columns([this.pedidos[i].estatus]).end,
-          new Columns([this.pedidos[i].metodoPago]).end,
-          new Columns([this.pedidos[i].direccionEnvio]).end,
-          new Columns([this.pedidos[i].fechaEntrega]).fontSize(10).end]
-        ]).end
-      )
-  
-      pdf.add(
-        new Txt('Productos del pedido').bold().alignment('left').fontSize(12).margin([0,5,0,0]).end
-      )
-  
-      pdf.add(
-        new Columns(
-          ['Codigo',
-          'Precio',
-          'Cantidad',
-          'Monto']).fontSize(12).margin([0,10,0,0]).bold().alignment('left').end
-        
-      )
-      console.log(this.pedidos[i].tiene);
-      for (let j = 0; j < this.pedidos[i].tiene.length; j++) {
-
-        /*pdf.add(
+    
+    if(this.filtro=="Todas"){
+      
+      for(let i=0;i<this.pedidos.length;i++){
+        pdf.add(
           new Table([
-            [new Columns([this.pedidos[i].tiene[j].codigoProd]).end,
-            new Columns([this.pedidos[i].tiene[j].precioProd]).end,
-            new Columns([this.pedidos[i].tiene[j].cantidadProd]).end,
-            new Columns([this.pedidos[i].tiene[j].monto]).end
-          ]
+            [ new Txt('Estatus').bold().end,
+            new Txt('Metodo de pago').bold().end,
+            new Txt('Direccion').bold().end,
+            new Txt('Fecha entrega').bold().end],
+            [ new Columns([this.pedidos[i].estatus]).end,
+            new Columns([this.pedidos[i].metodoPago]).end,
+            new Columns([this.pedidos[i].direccionEnvio]).end,
+            new Columns([this.pedidos[i].fechaEntrega]).fontSize(10).end]
           ]).end
-        );*/
+        )
+    
+        pdf.add(
+          new Txt('Productos del pedido').bold().alignment('left').fontSize(12).margin([0,5,0,0]).end
+        )
+    
         pdf.add(
           new Columns(
-            [ this.pedidos[i].tiene[j].codigoProd,
-            this.pedidos[i].tiene[j].precioProd,
-            this.pedidos[i].tiene[j].cantidadProd,
-            this.pedidos[i].tiene[j].monto
-          ]).fontSize(10).alignment('left').end
+            ['Codigo',
+            'Precio',
+            'Cantidad',
+            'Monto']).fontSize(12).margin([0,10,0,0]).bold().alignment('left').end
         )
-      }
-      pdf.add(
-        new Txt('Total: $' + this.pedidos[i].total).bold().alignment("right").fontSize(14).margin([0,20,0,0]).end
-      )
-      pdf.add(
-        pdf.ln(4)
-      );
-    }
 
+        console.log(this.pedidos[i].tiene);
+        for (let j = 0; j < this.pedidos[i].tiene.length; j++) {
+          pdf.add(
+            new Columns(
+              [ this.pedidos[i].tiene[j].codigoProd,
+              this.pedidos[i].tiene[j].precioProd,
+              this.pedidos[i].tiene[j].cantidadProd,
+              this.pedidos[i].tiene[j].monto
+            ]).fontSize(10).alignment('left').end
+          )
+        }
+        pdf.add(
+          new Txt('Total: $' + this.pedidos[i].total).bold().alignment("right").fontSize(14).margin([0,20,0,0]).end
+        )
+        pdf.add(
+          pdf.ln(4)
+        );
+      }
+    }else{
+      //aqui imprime dependiendo el filtro
+      for(let k=0;k<this.pedidos.length;k++){
+        
+        if(this.pedidos[k].estatus==this.filtro){
+
+          pdf.add(
+            new Table([
+              [ new Txt('Estatus').bold().end,
+              new Txt('Metodo de pago').bold().end,
+              new Txt('Direccion').bold().end,
+              new Txt('Fecha entrega').bold().end],
+              [ new Columns([this.pedidos[k].estatus]).end,
+              new Columns([this.pedidos[k].metodoPago]).end,
+              new Columns([this.pedidos[k].direccionEnvio]).end,
+              new Columns([this.pedidos[k].fechaEntrega]).fontSize(10).end]
+            ]).end
+          )
+      
+          pdf.add(
+            new Txt('Productos del pedido').bold().alignment('left').fontSize(12).margin([0,5,0,0]).end
+          )
+      
+          pdf.add(
+            new Columns(
+              ['Codigo',
+              'Precio',
+              'Cantidad',
+              'Monto']).fontSize(12).margin([0,10,0,0]).bold().alignment('left').end
+          )
+  
+          console.log(this.pedidos[k].tiene);
+          for (let j = 0; j < this.pedidos[k].tiene.length; j++) {
+            pdf.add(
+              new Columns(
+                [ this.pedidos[k].tiene[j].codigoProd,
+                this.pedidos[k].tiene[j].precioProd,
+                this.pedidos[k].tiene[j].cantidadProd,
+                this.pedidos[k].tiene[j].monto
+              ]).fontSize(10).alignment('left').end
+            )
+          }
+          pdf.add(
+            new Txt('Total: $' + this.pedidos[k].total).bold().alignment("right").fontSize(14).margin([0,20,0,0]).end
+          )
+          pdf.add(
+            pdf.ln(4)
+          );
+
+        }
+      }
+    }
     pdf.create().download();
 
     pdf.create().open();
