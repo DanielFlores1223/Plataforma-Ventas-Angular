e457ed6c1526b7722f684ed927fe5304e756539a
dontpan-ik <64174091+dontpan-ik@users.noreply.github.com>
2020-11-30 21:20:43 -0600

    new updates

--- src/app/components/Empleado/reportes/reportes.component.ts
+++ src/app/components/Empleado/reportes/reportes.component.ts
@@ -6,7 +6,6 @@ import { PdfMakeWrapper, Txt, Img, Columns, Stack, Table, Cell } from 'pdfmake-w
 import {PedidoService} from '../../../services/pedido.service';
 import {ClienteService} from '../../../services/cliente.service';
 import { InventarioService } from '../../../services/inventario.service';
-import { now } from 'moment';
 
 @Component({
   selector: 'app-reportes',
@@ -15,16 +14,18 @@ import { now } from 'moment';
 })
 export class ReportesComponent implements OnInit {
 
+  isCheck=false;
+  byDate;
   pedidos;
 
-  dateHoy= new Date();
+  dateHoy = new Date();
 
   //dateHoy: number = Date.now();
 
   noPedidos=0;
   
   total=0;
-  filtro="Todas";
+  filtro="Todos";
   tipo="Total";
 
   periodo =["dia","semana","mes","anio"];
@@ -54,7 +55,8 @@ export class ReportesComponent implements OnInit {
     img : ""
   }
 
-  constructor(private pedidoService : PedidoService, private productoService : InventarioService) { }
+  constructor(private pedidoService : PedidoService, private productoService : InventarioService) {
+   }
 
   ngOnInit(): void {
     this.totalPedidos()
@@ -109,7 +111,7 @@ export class ReportesComponent implements OnInit {
     this.total=0;
     this.noPedidos=0;
     // it could be some trouble here, just be careful
-    if(this.filtro!="Todas"){
+    if(this.filtro!="Todos"){
       for(let i=0;i<this.pedidos.length;i++){
         if(this.pedidos[i].estatus==this.filtro){
           this.noPedidos++;
@@ -130,10 +132,26 @@ export class ReportesComponent implements OnInit {
     }
   }
 
-  generarPDF(){       
-    const pdf = new PdfMakeWrapper();
+  changeStatus(){
+    if(this.isCheck==true){
+      this.isCheck=false;
+    }else{
+      this.isCheck=true;
+    }
+  }
 
+  generarPDF(){
+    console.log(this.dateHoy);
+    //var fecha_nueva='fecha: '+this.dateHoy.getDay()+'/'+this.dateHoy.getMonth()+'/'+this.dateHoy.getUTCFullYear();
+    var fecha_nueva= this.dateHoy.toString();
+    console.log(fecha_nueva)
+    var num=0;       
+    const pdf = new PdfMakeWrapper();
     pdf.pageSize('A5');
+    //'fecha: '+this.dateHoy.getDay()+'/'+date.getMonth()+'/'+date.getUTCFullYear()
+    pdf.add(
+      new Txt(fecha_nueva).bold().alignment('right').fontSize(5).margin([0,5,0,0]).end
+    )
     
     new Img('../assets/img/logo_crem_adap.png').alignment('center').height(80).width(80).build().then( img => {
     pdf.add( img );
@@ -146,11 +164,17 @@ export class ReportesComponent implements OnInit {
     );
     pdf.add(pdf.ln());
     
-    if(this.filtro=="Todas"){
+    if(this.filtro=="Todos"){
       
       for(let i=0;i<this.pedidos.length;i++){
 
         if(this.pedidos[i].estatus!="en carrito"){
+
+          num++;
+
+          pdf.add(
+            new Txt("Pedido no° "+num.toString()).alignment('left').fontSize(10).bold().end
+          );
 
           pdf.add(
             new Table([
@@ -179,6 +203,7 @@ export class ReportesComponent implements OnInit {
   
           console.log(this.pedidos[i].tiene);
           for (let j = 0; j < this.pedidos[i].tiene.length; j++) {
+
             pdf.add(
               new Columns(
                 [ this.pedidos[i].tiene[j].codigoProd,
@@ -202,6 +227,12 @@ export class ReportesComponent implements OnInit {
       for(let k=0;k<this.pedidos.length;k++){
         
         if(this.pedidos[k].estatus==this.filtro){
+
+          num++;
+
+          pdf.add(
+            new Txt("Pedido no° "+num.toString()).alignment('left').fontSize(10).bold().end
+          );
 
           pdf.add(
             new Table([
